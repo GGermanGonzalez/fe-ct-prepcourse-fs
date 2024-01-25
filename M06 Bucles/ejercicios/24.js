2d20cfa6780dcd62ba8b280aa193c9beda539d86
GGermanGonzalez <negerman@hotmail.com.ar>
2024-01-25 17:13:13 -0300

    ejercicio 24

--- M06 Bucles/ejercicios/24.js
+++ M06 Bucles/ejercicios/24.js
@@ -1,7 +1,13 @@
 function invertirTexto(texto) {
-  // La función recibe un argumento "texto" que es un string.
-  // Tu objetivo es invertir el string y devolver el string invertido.
+//   La función recibe un argumento "texto" que es un string.
+ //  Tu objetivo es invertir el string y devolver el string invertido.
   // Tu código:
-}
+ // var palabraFinal=""
+// for(var i= texto.length - 1;i>=0;i--){
 
+  //palabraFinal=palabraFinal+texto[i]
+ return texto.split("").reverse().join("")
+  }
+ // return palabraFinal
+//}
 module.exports = invertirTexto;
